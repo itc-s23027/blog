ff5ff1791fd2f4a17df86c1289c02c4550e63f9a
itc-s23027 <s23027@std.it-college.ac.jp>
2024-01-18 07:07:51 +0000

    ch8

--- components/Next.js
+++
@@ -1,9 +0,0 @@
-import Link from 'next/link'
-
-export default function Test() {
-  return (
-    <Link href="/">
-      <a>トップページを開く</a>
-    </Link>
-  )
-}
--- components/post-body.js
+++
@@ -1,5 +0,0 @@
-import styles from 'styles/post-body.module.css'
-const PostBody = ({ children }) => {
-  return <div className={styles.stack}>{children}</div>
-}
-export default PostBody
