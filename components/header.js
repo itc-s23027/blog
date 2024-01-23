959f30fc84582e571580abc2750a57ba68297571
itc-s23027 <s23027@std.it-college.ac.jp>
2024-01-23 06:52:14 +0000

    ch3

---
+++ components/header.js
@@ -0,0 +1,12 @@
+import Logo from 'components/logo'
+import Nav from 'components/nav'
+
+const Header = () => {
+  return (
+    <header>
+      <Logo boxOn />
+      <Nav />
+    </header>
+  )
+}
+export default Header
---
+++ pages/blog/index.js
@@ -0,0 +1,6 @@
+import Hero from 'components/hero'
+
+const Blog = () => {
+  return <Hero title='Blog' subtitle='Recent Posts' />
+}
+export default Blog
